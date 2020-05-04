92661f414e3e7d14aa15f81aa71cb8fb644cd2c3
Daniel Høgli Olufsen <daniel@katori.no>
2020-05-04 23:13:01 +0200

    init commit

---
+++ src/main/java/com/sysbitno/futsal/FutsalTournamentApplication.java
@@ -0,0 +1,13 @@
+package com.sysbitno.futsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsalTournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutsalTournamentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysbitno/futsal/FutsalTournamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysbitno.futsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutsalTournamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
